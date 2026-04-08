0313e6118d2ef831ec4f0c7fe0e13e592b6ee128
MagnusAleksandar <alevel.col@gmail.com>
2026-04-08 09:52:15 -0500

    Agregados folders, Data Class 'Jugador'

---
+++ app/src/main/java/com/example/monopoli/data/Player.kt
@@ -0,0 +1,4 @@
+package com.example.monopoli.data
+
+data class Player(val name: String, var money: Int, var playing: Boolean)
+                //Nombre,           dinero,         si es su turno o no

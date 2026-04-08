2a10f3328adf1954c014b7eac4d36d002e355b63
MagnusAleksandar <alevel.col@gmail.com>
2026-04-08 10:36:04 -0500

    Lógica para invertir y gastar agregada

--- app/src/main/java/com/example/monopoli/model/Turn.kt
+++ app/src/main/java/com/example/monopoli/model/Turn.kt
@@ -1,12 +1,33 @@
 package com.example.monopoli.model
 
 import com.example.monopoli.data.Player
+import kotlin.random.Random
 
-class Turn (val player: Player, val interest: Float){
+class Turn (val player: Player, val interest: Float, val spent: Float){
+    //      Jugador,            interés del ahorro,  valor gastado
+    val possOutcomes = listOf<Char>('g', 'l') // Resultados posibles de invertir (ganar: gain - g, perder: loss - l)
     var mon = player.money
 
     fun save(){
-        mon = mon + mon * interest
+        mon =+ mon * interest
+
+    }
+
+    fun invest(){
+        val outcome = possOutcomes.random()
+        val chng = Random.nextDouble(0.01, 1.00).toFloat()
+
+        when (outcome){
+            'g' -> mon =+ mon * chng
+            'l' -> mon =- mon * chng
+            else -> print("Acción no válida")
+
+        }
+
+    }
+
+    fun spend(){
+        mon =- spent
 
     }
 

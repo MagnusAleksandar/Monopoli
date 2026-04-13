8f1315d43e0183e633d7257f6c3b34beb101403d
MagnusAleksandar <alevel.col@gmail.com>
2026-04-13 09:59:59 -0500

    Mejorada la lógica del juego

--- app/src/main/java/com/example/monopoli/model/Turn.kt
+++ app/src/main/java/com/example/monopoli/model/Turn.kt
@@ -3,29 +3,33 @@ package com.example.monopoli.model
 import com.example.monopoli.data.Player
 import kotlin.random.Random
 
-class Turn (val player: Player, val interest: Float?, val spent: Float?){
+class Turn (private val player: Player, private val interest: Float?, private val spent: Float?){
     //      Jugador,            interés del ahorro,  valor gastado
     val possOutcomes = listOf<Char>('g', 'l') // Resultados posibles de invertir (ganar: gain - g, perder: loss - l)
-    var mon = player.money
+//    var mon = player.money
 
-    fun save(){
-        mon += mon * (interest ?: 0f)
+    fun save(): Player{ // Ahorrar
+        return player.copy(money = player.money * (interest ?: 0f))
     }
 
-    fun invest(){
+    fun monUpDown(): Player{ // Cambios al monto de dinero del jugador (invertir, eventos aleatorios)
         val outcome = possOutcomes.random()
-        val chng = Random.nextDouble(0.01, 1.00).toFloat()
+        val chng: Float
 
-        when (outcome){
-            'g' -> mon += mon * chng
-            'l' -> mon -= mon * chng
-            else -> println("Acción no válida")
+        if (interest == null){
+            chng = player.money * Random.nextDouble(0.01, 1.00).toFloat()
+        }else{
+            chng = player.money * interest
+        }
 
+        when (outcome) {
+            'g' -> return player.copy(money = player.money + chng)
+            'l' -> return player.copy(money = player.money - chng)
+            else -> return player.copy()
         }
     }
 
-    fun spend(){
-        mon -= (spent ?: 0f)
+    fun spend(): Player{ // Gastar
+        return player.copy(money = player.money + (spent ?: 0f))
     }
-
 }
---
+++ app/src/main/java/com/example/monopoli/viewModel/GameViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.monopoli.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.monopoli.data.Player
+import com.example.monopoli.model.Turn
+
+class GameViewModel: ViewModel(){
+    private val INTEREST = .15f
+    private val _player = MutableLiveData<Player>(Player("Player"))
+    val player: LiveData<Player> = _player
+
+    private fun turn(inc: Float? = null) =  Turn(_player.value!!, INTEREST, inc)
+
+    fun onSave(inc: Float){
+        _player.value = turn(inc).save()
+    }
+
+}
